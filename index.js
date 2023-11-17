a51b2a68fcd603df5b9e6bb0df94d4c8d8872be1
Mohammed Subhan Ayaz Zama <ayazzama786@gmail.com>
2023-11-17 23:54:57 +0530

    changes

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());

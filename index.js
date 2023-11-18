64bb4c0f1e322c8aa2b8ec0eaf0c6db4b692f0e4
Mohammed Subhan Ayaz Zama <ayazzama786@gmail.com>
2023-11-18 15:58:41 +0530

    Update

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
+const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
@@ -14,3 +15,7 @@ mongoose.connect(process.env.MONGODB_URI, {
 
 const userRoutes = require('./routes/userRoutes');
 app.use('/api/users', userRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
